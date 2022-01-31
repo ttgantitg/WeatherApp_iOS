922a84dc9f01d6eeffff827fe7b1187c8cd08573
ttgantitg <ttgantitg@gmail.com>
2022-01-31 20:12:06 +0300

    Initial Commit

---
+++ Clima/WeatherData.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by ttgantitg on 31.01.2022.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let description: String
+    let id: Int
+}
